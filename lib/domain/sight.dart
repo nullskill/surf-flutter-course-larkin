deea52f93ab3cafefe10682b1d7c02c1deeb820f
nullskill <nullskill@github.com>
2020-12-09 17:06:20 +0300

    Add mockup for sights

--- lib/domain/sight.dart
+++ lib/domain/sight.dart
@@ -1,10 +1,9 @@
 class Sight {
-  final String sightName;
+  final String name;
   final double lan, lon;
   final String url;
   final String details;
   final String type;
 
-  Sight(
-      {this.sightName, this.lan, this.lon, this.url, this.details, this.type});
+  Sight({this.name, this.lan, this.lon, this.url, this.details, this.type});
 }
--- lib/mocks.dart
+++ lib/mocks.dart
@@ -1,1 +1,35 @@
-final List mocks = [];
+import 'package:places/domain/sight.dart';
+
+final List mocks = [
+  Sight(
+      name: "Центр современного искусства «Винзавод»",
+      lan: 55.75569,
+      lon: 37.66458,
+      url:
+          "https://kudago.com/media/thumbs/xl/images/place/04/bf/04bf605cb52a1ad2bf1502ff864736fd.jpg",
+      details: "Комплекс старинных павильонов постройки начала XIX века"
+          "\nсостоит из галерей, двух десятков магазинов,"
+          "\nобразовательных мастерских и необычных кафе.",
+      type: "artPlace"),
+  Sight(
+      name: "Центр творческих индустрий «Фабрика»",
+      lan: 55.77908,
+      lon: 37.68887,
+      url:
+          "https://kudago.com/media/thumbs/xl/images/place/0d/1c/0d1cd06407b0e88b865a5e45ba7aec0b.jpg",
+      details: "В центре «Фабрика», объединяющем выставочные пространства,"
+          "\nтеатральные подмостки, мастерские кино и музыки,"
+          "\nрегулярно и практически без перерыва проходят"
+          "\nбесплатные выставки современных художников.",
+      type: "artPlace"),
+  Sight(
+      name: "Смотровая площадка PANORAMA360",
+      lan: 55.74985,
+      lon: 37.53772,
+      url:
+          "https://kudago.com/media/thumbs/xl/images/place/12/af/12afdd8bc989a84bdd0f6373b15f7c5f.jpg",
+      details: "Здесь можно полюбоваться невероятными видами Москвы,"
+          "\nпосетить фабрику мороженого или шоколада,"
+          "\nа также заглянуть в другие развлекательные зоны.",
+      type: "showPlace"),
+];
