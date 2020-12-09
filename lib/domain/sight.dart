70c72d98de9517641719b1667a65fa9a9d220912
ilya <ilya@MacBook-Air-ilya.local>
2020-12-09 09:27:58 +0300

    Add mockup structure

---
+++ lib/domain/sight.dart
@@ -0,0 +1,10 @@
+class Sight {
+  final String sightName;
+  final double lan, lon;
+  final String url;
+  final String details;
+  final String type;
+
+  Sight(
+      {this.sightName, this.lan, this.lon, this.url, this.details, this.type});
+}
