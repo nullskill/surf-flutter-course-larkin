e8674b882709bfcb4b834e0de1580185bf5ec7ac
nullskill <nullskill@github.com>
2021-01-23 20:40:51 +0300

    Add dummy SightSearchScreen

---
+++ lib/ui/screens/sight-search-screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+/// Экран поиска интересного места.
+class SightSearchScreen extends StatefulWidget {
+  @override
+  _SightSearchScreenState createState() => _SightSearchScreenState();
+}
+
+class _SightSearchScreenState extends State<SightSearchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
